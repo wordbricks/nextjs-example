a613e078741fad6dd1d9706f5d36b6aa1b6e3a7e
Wordbricks <123422550+wordbricks-dev@users.noreply.github.com>
2023-10-08 21:18:51 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import styles from '@/styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.description}>
-      <h3>Welcome to taho</h3>
+      <h3>Welcome to Wordbricks</h3>
     </div>
   )
 }
