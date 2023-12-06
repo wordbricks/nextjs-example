1018e602512378ea9f8be0a6bf1a6c07de435a08
Jay Nam <siisee111@gmail.com>
2023-12-06 10:43:39 +0900

    feat: minimum template

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,9 +1,57 @@
-import styles from '@/styles/Home.module.css'
+import { useState } from 'react';
+
+type History = {
+  input: string;
+  output: string;
+};
 
 export default function Home() {
+
+  const [inputText, setInputText] = useState('');
+  const [reversedText, setReversedText] = useState('');
+  const [history, setHistory] = useState<History[]>([]);
+
+
+  const handleReverseText = async () => {
+    try {
+    //   const response = await fetch('http://localhost:8000/text/reverse', {
+    //     ...
+    // }
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  };
+
+  const handleLoadHistory = async () => {
+    try {
+      // const response = await fetch('http://localhost:8000/text/history');
+      // ...
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  };
+
   return (
-    <div className={styles.description}>
-      <h3>Welcome to Wordbricks</h3>
+    <div>
+      <h1>Test Reverse Text</h1>
+      <textarea
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+        placeholder="Enter text here"
+      ></textarea>
+      <button onClick={handleReverseText}>Reverse Text</button>
+      <textarea
+        value={reversedText}
+        readOnly
+        placeholder="Reversed text will appear here"
+      ></textarea>
+      <button onClick={handleLoadHistory}>Load History</button>
+      <ul>
+        {history.map((entry, index) => (
+          <li key={index}>{`Input: ${entry.input}, Output: ${entry.output}`}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
+
 }
